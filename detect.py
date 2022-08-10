e0804c9ab2191ea500e1f7b093b96ac4cbb3322a
Amisi Kiarie <8081220+akiarie@users.noreply.github.com>
2022-08-10 22:17:02 +0300

    detection works

--- detect.py
+++ detect.py
@@ -20,86 +20,93 @@ coords = {
         "left": [],
         "right": []
     },
-    "disputes": {
-        "left": [],
-        "right": []
-    },
 }
 
 def permit(s, nextst):
     def _permit(result):
-        if result["text"] == s:
+        if result["text"].startswith(s):
             return nextst, True
-        return nextst(result)
+        nextst2, should_draw = nextst(result)
+        if should_draw:
+            return nextst2, True
+
+        return _permit, False
+    return _permit
 
 def st_right_title(result):
-    if result["text"] == "STATION":
+    if result["text"].startswith("STATION"):
         coords["title"]["right"] += [result]
         return st_candidates, True
 
-    if result["text"] == "POLLING":
+    if result["text"].startswith("POLLING"):
         coords["title"]["right"] += [result]
         return permit("STATION", st_candidates), True
 
     return st_right_title, False
 
 def st_title(result):
-    if result["text"] == "ELECTION":
+    if result["text"].startswith("ELECTION"):
         coords["title"]["left"] += [result]
         return st_right_title, True
 
-    if result["text"] == "PRESIDENTIAL":
+    if result["text"].startswith("PRESIDENTIAL"):
         coords["title"]["left"] += [result]
         return permit("ELECTION", st_right_title), True
 
     return st_title, False
 
 def st_george(result):
-    if result["text"] == "GEORGE":
+    if result["text"].startswith("GEORGE"):
         coords["candidates"] += [result]
         return st_stats, True
     return st_george, False
 
 def st_candidates(result):
-    if result["text"] == "WILLIAM":
+    if result["text"].startswith("WILLIAM"):
         coords["candidates"] += [result]
         return permit("DAVID", st_george), True
 
-    if result["text"] == "DAVID":
+    if result["text"].startswith("DAVID"):
         coords["candidates"] += [result]
         return st_george, True
 
     return st_candidates, False
 
+def st_votes(result):
+    for s in ["votes", "if", "any"]:
+        if result["text"].startswith(s):
+            return None, False # no printing at the end
+    return st_votes, False
 
 def st_disputes(result):
-    if result["text"] == "Decision":
-        coords["dispute"] += [result]
-        return permit("disputed", None), True
+    if result["text"].startswith("Decision"):
+        coords["stats"]["right"] += [result]
+        return permit("dispute", st_votes), True
 
-    if result["text"] == "disputed":
-        coords["dispute"] += [result]
-        return None, True
+    if result["text"].startswith("dispute"):
+        coords["stats"]["right"] += [result]
+        return st_votes, True
 
     return st_disputes, False
 
 def st_stats(result):
-    if result["text"] == "Polling":
-        coords["stats"] += [result]
+    if result["text"].startswith("Polling"):
+        coords["stats"]["left"] += [result]
         return permit("Station", st_disputes), True
 
-    if result["text"] == "Station":
-        coords["stats"] += [result]
+    if result["text"].startswith("Station"):
+        coords["stats"]["left"] += [result]
         return st_disputes, True
 
     return st_stats, False
 
-state = st_title
 
 def draw(image, x, y, w, h):
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
     cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
 
+
+state = st_title
 
 # loop over each of the individual text localizations
 for i in range(0, len(results["text"])):
@@ -128,13 +135,11 @@ for i in range(0, len(results["text"])):
         "conf": conf,
     }
 
-    nextst, should_draw = state(result)
+    state, should_draw = state(result)
     if should_draw:
         x, y = result["coords"]["x"], result["coords"]["y"]
         w, h = result["dimensions"]["w"], result["dimensions"]["h"]
         draw(image, x, y, w, h)
-
-    state = nextst
 
     if state is None:
         break
